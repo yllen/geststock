c0c10bd632dbbf7ef9b0c61d3b11befda269b4dc
yllen <nini.lasson@orange.fr>
2018-04-18 18:03:11 +0200

    update header

--- hook.php
+++ hook.php
@@ -1,6 +1,5 @@
 <?php
 /*
- * @version $Id: $
  -------------------------------------------------------------------------
  LICENSE
 
@@ -21,7 +20,7 @@
 
  @package   geststock
  @author    Nelly Mahu-Lasson
- @copyright Copyright (c) 2017 GestStock plugin team
+ @copyright Copyright (c) 2017-2018 GestStock plugin team
  @license   AGPL License 3.0 or (at your option) any later version
             http://www.gnu.org/licenses/agpl-3.0-standalone.html
  @link
@@ -142,7 +141,6 @@ function plugin_geststock_giveItem($type, $ID, $data, $num) {
                    && ($type == "PluginSimcardSimcard")) {
                  $table = 'glpi_plugin_simcard_simcardtypes';
                }
-               if (!empty($table)) {
                   $query = "SELECT `".$table."`.`$column`, `nbrereserv`
                             FROM `glpi_plugin_geststock_reservations_items`, `".$table."`
                             WHERE `".$table."`.`id`
@@ -197,7 +195,6 @@ function plugin_geststock_giveItem($type, $ID, $data, $num) {
 }
 
 
-
 function plugin_geststock_postinit() {
    global $PLUGIN_HOOKS;
 
@@ -210,6 +207,7 @@ function plugin_geststock_postinit() {
       Plugin::registerClass('PluginGeststockSpecification', ['addtabon' => $mod]);
    }
 }
+
 
 function plugin_geststock_getAddSearchOptions($itemtype) {
    global $CFG_GLPI;
